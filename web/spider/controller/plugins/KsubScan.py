bad472869f1a1f59948af56abdc1f2ea2498f8d7
LoRexxar <lorexxar@gmail.com>
2023-02-15 21:25:00 +0800

    add subdomain check in database

--- web/spider/controller/plugins/KsubScan.py
+++ web/spider/controller/plugins/KsubScan.py
@@ -29,8 +29,7 @@ class KsubScan:
     def query(self, domain, deep=0):
 
         try:
-            print(" ".join(["echo", domain, "|", self.kspath, "enum", "--stdin", "--silent"]))
-            p = subprocess.Popen(["echo", domain, "|", "sudo", self.kspath, "enum", "--stdin", "--silent"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p = subprocess.Popen(["echo", domain, "|", "sudo", self.kspath, "enum", "--stdin", "--silent"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         except:
             logger.warning("[Pre Scan][KsubScan] ksubdomain {} scan error.".format(domain))
